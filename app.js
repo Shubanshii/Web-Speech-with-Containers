61f597824e0bf467a46e8995ea282fb52841a082
Shubanshii <c_day@alumni.baylor.edu>
2017-04-10 16:13:00 +0700

    updates with div hiding and showing

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
-console.log("working");
 $(document).ready(function(){
+		
 	$(".submitButton").on("click", function() {
+		divCount = $("#results").text().split(" ").length;
 		$("#info, .right, #results, .center, button").hide();
 		console.log($("#results").text().split(" ").length); 
-		var divCount = $("#results").text().split(" ").length;	
+		
 		var container = $('<div />');
 		for (i = 1; i <= divCount; i++) { 
-			container.append('<div class="Div ' + i + '">Div ' + i + '</div>');
-
+			container.append('<div class="Div-' + i + '">Div ' + i + '</div>');
+			
 		}
+
 		$('div').html(container);
+		
+		
+		next();
 
 	})
+
+	 function next(){
+	 	var i = 1;
+	 	$('body').append('<button class="nextButton">Next</button>');
+	for(i = 1; i<=divCount; i++){
+	if(i>1){
+			$(".Div-" + i + "").hide();
+		}}
+		i=1;
+		$(".nextButton").on("click", function(){
+			
+			$(".Div-" + i + "").hide();
+			
+			$(".Div-" + (i+1) + "").show();
+			i++;
+		 })
+	}
+
 	     
 
 })
