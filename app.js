43b71d7841f412c268cceae2135e5c5d8b690d75
Shubanshii <c_day@alumni.baylor.edu>
2017-04-14 13:48:06 +0700

    struggling to figure out how I want to get the words component to work

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
-		
+	
 	$(".submitButton").on("click", function() {
+		var res = $("#results").text().split(" ");
+		console.log(res);
+
+		$.ajax({
+
+    url: 'https://wordsapiv1.p.mashape.com/words/' + $("#results").text() + '', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
+    type: 'GET', // The HTTP Method
+    data: {}, // Additional parameters here
+    datatype: 'json',
+    success: function(data) {  
+    	console.log(data);
+    	console.log(data.results[0].definition);
+
+    },
+    error: function(err) { alert(err); },
+    beforeSend: function(xhr) {
+    xhr.setRequestHeader("X-Mashape-Authorization", "5QNhUGUmVamshQCbuFO6ykRJBCqFp1nqhQgjsnNahs0JInCns7"); // Enter here your Mashape key
+    
+    }
+});
 		divCount = $("#results").text().split(" ").length;
 		$("#info, .right, #results, .center, button").hide();
 		console.log($("#results").text().split(" ").length); 
@@ -12,6 +32,7 @@ $(document).ready(function(){
 		}
 
 		$('div').html(container);
+		
 		
 		
 		next();
